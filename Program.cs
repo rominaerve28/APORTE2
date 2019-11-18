ee584937c98c30e8ae5a08bdf465f06fb95bf333
Romina Eras <56611788+rominaerve28@users.noreply.github.com>
2019-11-18 10:58:07 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,9 +10,7 @@ namespace aporte2
     {
         static void Main(string[] args)
         {
-            List<Reserva> listareserva = new List<Reserva>();
-            Cliente.Add("Reserva1");
-            Cliente.Add("Reserva2");
+       
 
             Cliente cliente = new Cliente("Identificacion : 13026304551", "Nombre: Francisco Campuzano", ) ;
             Reserva reserva = new Reserva( "" );
